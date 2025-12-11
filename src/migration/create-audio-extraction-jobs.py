f58638a0c6c0c38d496d6c6bc61f8b9cb0dbe068
Naren Magar <nagendra.magar@varosatech.com>
2025-12-11 21:38:42 +0545

    add down migration

--- src/migration/create-audio-extraction-jobs.py
+++ src/migration/create-audio-extraction-jobs.py
@@ -2,6 +2,6 @@ from src.database import DbEngine
 from src.models import base_model, audio_extraction_job_model
 
 
-print("Creating table")
+print("Creating table audio_extraction_jobs...")
 base_model.BaseModel.metadata.create_all(bind=DbEngine().engine)
-print("Completed")
+print("Completed...")
